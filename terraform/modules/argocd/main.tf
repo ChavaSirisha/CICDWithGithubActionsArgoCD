9d8e1455f312433b78364cbe395db22e244b44e6
anitha kumari Patcha <anitha.patcha17@gmail.com>
2025-05-20 17:36:04 -0400

    updated modules

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -14,6 +14,6 @@ resource "helm_release" "argocd" {   //install the ArgoCD Helm chart
   create_namespace = true
 
   values = [
-    file("${path.module}/values.yaml")  //loads custom Helm values from a values.yaml file
+    file("${path.module}/Values.yaml")  //loads custom Helm values from a values.yaml file
   ]
 }
